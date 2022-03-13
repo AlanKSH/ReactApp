b441b8229b9f4569e409ac23081b103e2086f97f
Sara Ngo <sara.ngoo.9641@gmail.com>
2022-03-13 15:40:31 -0700

    Create Route to Rider, Driver, Login page
    -templates: contains all pages
    -styles: contains all css files
    -components: contain all micro-services
    -move map to Rider page
    -create navbar for Homepage.

---
+++ frontend/src/templates/Rider.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../styles/App.css'
+import Map from '../components/Map'
+
+function Rider() {
+  return (
+    <div>
+        <p>Rider</p>
+        <Map/>
+    </div>
+
+  );
+}
+
+export default Rider;
